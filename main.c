2596ba7cd1254b300b51ad47b5d0d5b59e8f1092
Othmane Touti <ottouti@c3r4p6.42quebec.com>
2024-01-29 13:04:35 -0500

    implemented fork setting

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:56:12 by ottouti           #+#    #+#             */
-/*   Updated: 2024/01/29 12:42:18 by ottouti          ###   ########.fr       */
+/*   Updated: 2024/01/29 13:03:45 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,17 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	table = set_table(argc, argv);
-	printf("Number of Philosophers: %ld\n"
-		"Time to die: %ld\n"
-		"Time to eat: %ld\n"
-		"Time to sleep: %ld\n"
-		"Max meals: %ld\n"
-		"Start time: %ld\n", table.philo_nbr, table.time_to_die,
-		table.time_to_eat, table.time_to_sleep, table.meal_max,
-		table.start_time);
+	// printf("Number of Philosophers: %ld\n"
+	// 	"Time to die: %ld\n"
+	// 	"Time to eat: %ld\n"
+	// 	"Time to sleep: %ld\n"
+	// 	"Max meals: %ld\n"
+	// 	"Start time: %ld\n", table.philo_nbr, table.time_to_die,
+	// 	table.time_to_eat, table.time_to_sleep, table.meal_max,
+	// 	table.start_time);
+	for (int i = 0; i < table.philo_nbr; i++)
+	{
+		printf("Fork id = %d\n", table.forks[i].fork_id);
+	}
 	return (0);
 }
--- src/init.c
+++ src/init.c
@@ -6,19 +6,35 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 12:23:08 by ottouti           #+#    #+#             */
-/*   Updated: 2024/01/29 12:43:18 by ottouti          ###   ########.fr       */
+/*   Updated: 2024/01/29 13:02:56 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/philo.h"
 
+static t_fork	*place_forks(int philo_nbr)
+{
+	t_fork	*forks;
+	int		i;
 
+	i = 0;
+	forks = malloc(sizeof(t_fork) * philo_nbr);
+	if (!forks)
+		return (NULL);
+	while (i < philo_nbr)
+	{
+		forks[i].fork_id = i;
+		pthread_mutex_init(&forks[i].fork, NULL);
+		i++;
+	}
+	return (forks);
+}
 static long get_time(void)
 {
 	struct timeval	time;
 
 	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000000 + time.tv_usec);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
 t_table set_table(int argc, char **argv)
@@ -35,5 +51,8 @@ t_table set_table(int argc, char **argv)
 		table.meal_max = -1;
 	table.start_time = get_time();
 	table.end = false;
+	table.forks = place_forks(table.philo_nbr);
+	if (!table.forks)
+		return (table);
 	return (table);
 }
